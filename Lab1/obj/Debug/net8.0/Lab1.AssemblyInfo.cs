981a86628e9e5bc47ab4d7059bd81fe58535bc6e
onurqnel <onur.onel2230@gmail.com>
2024-09-04 02:24:15 -0400

    Lab 1 Finished

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1682a7743f824bcfd804d8eef90b6ed3bae52cb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca8f5f234e76b995c5b5e7d34f5160b4f791c7e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

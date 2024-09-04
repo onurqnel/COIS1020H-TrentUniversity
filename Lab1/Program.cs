29e0bf6eee87de9c94b6f5065458475bf7d3051e
onurqnel <onur.onel2230@gmail.com>
2024-09-04 01:36:22 -0400

    Lab 1 Done

---
+++ Lab1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public static class Lab1
+{
+    public static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Hello COIS1020H, my name is Onur Onel");
+    }
+}

b07e3503a7bd8706600ecc000f143c0479918ac9
onurqnel <onur.onel2230@gmail.com>
2024-09-26 19:50:29 -0400

    Lab 3 Done

---
+++ Lab3_1/Lab3_1.cs
@@ -0,0 +1,45 @@
+﻿/*
+* Name: Onur (Honor) Onel
+* Student Number: 0865803
+* Date: 2024-09-19
+* Lab 3, Part 1
+* Program Description: This program computes the amount of money in a
+* piggy-bank given the number of pennies, nickels, dimes, quarters,
+* loonies, and twoonies.
+*/
+public static class Lab3_1
+{
+    public static void Main()
+    {
+        // declare the variables and constants
+        int numPens, numNicks, numDimes, numQuarts, numLoons, numTwoons;
+        double amtInPiggyBank;
+        string name;
+        Console.Clear();
+        // Input the name of the user
+        Console.Write("Enter your name => ");
+        name = Console.ReadLine();
+        // Input the number of pennies
+        Console.Write("Enter the number of pennies in the Piggy Bank => ");
+        numPens = Convert.ToInt32(Console.ReadLine());
+        // Input the number of nickels
+        Console.Write("Enter the number of nickels in the Piggy Bank => ");
+        numNicks = Convert.ToInt32(Console.ReadLine());
+        // Input the number of dimes
+        Console.Write("Enter the number of dimes in the Piggy Bank => ");
+        numDimes = Convert.ToInt32(Console.ReadLine());
+        // Input the number of quarters
+        Console.Write("Enter the number of quarters in the Piggy Bank => ");
+        numQuarts = Convert.ToInt32(Console.ReadLine());
+        // Input the number of loonies
+        Console.Write("Enter the number of loonies in the Piggy Bank => ");
+        numLoons = Convert.ToInt32(Console.ReadLine());
+        // Input the number of twoonies
+        Console.Write("Enter the number of twoonies in the Piggy Bank => ");
+        numTwoons = Convert.ToInt32(Console.ReadLine());
+        // Compute the amount in the Piggy Bank
+        amtInPiggyBank = numPens * 0.01 + numNicks * 0.05 + numDimes * 0.10 + numQuarts * 0.25 + numLoons * 1 + numTwoons * 2;
+        // Print out the amount in the Piggy Bank
+        Console.WriteLine("{0} has {1:C} in the Piggy Bank", name, amtInPiggyBank);
+    }
+}
---
+++ Lab3_2/Lab3_2.cs
@@ -0,0 +1,53 @@
+﻿/*
+* Name: Onur (Honor) Onel
+* Student Number: 0865803
+* Date: 2024-09-19
+* Lab 3, Part 2
+* Program Description: This program calculates the cost of the package based on its weight and flat price
+*/
+public static class Lab3_2
+{
+   public static void Main()
+   {
+      // declare the variables and constants
+      const double FLAT = 1.25;
+      double weight;
+      double cost, pricePerKg = 0;
+
+      Console.Clear();
+      // Input the weight
+      Console.Write("Enter a positive weight of the package => ");
+      weight = Convert.ToDouble(Console.ReadLine());
+      // Determine the cost per kilogram
+      if (weight <= 0)
+      {
+         Console.WriteLine("*** Invalid weight");
+
+      }
+      else if (weight < 1)
+      {
+         pricePerKg = 0.25;
+
+      }
+      else if (weight >= 1 && weight <= 3.5)
+      {
+         pricePerKg = 0.5;
+      }
+      else if (weight > 3.5)
+      {
+         pricePerKg = 1.0;
+
+      }
+      // Compute the cost to send the package
+      if (weight > 0)
+      {
+         cost = weight * pricePerKg + FLAT;
+      }
+      else
+      {
+         cost = 0;
+      }
+      // Output the results
+      Console.WriteLine("The cost to send the package is {0:C}", cost);
+   }
+}

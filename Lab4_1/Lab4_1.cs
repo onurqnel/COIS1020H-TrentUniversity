a72a23c4d667799e198242fee1b91080781d8663
onurqnel <onur.onel2230@gmail.com>
2024-09-27 12:38:10 -0400

    Lab 4 Done

---
+++ Lab4_1/Lab4_1.cs
@@ -0,0 +1,43 @@
+﻿/*
+* Name: Onur (Honor) Onel
+* Student Number: 0865803
+* Date: 2024-09-27
+* Lab 4, Part 1
+*/
+public static class Lab4_1
+{
+  public static void Main()
+  {
+    int value = 0, sum = 0, count = 0;
+    double average = 0.0;
+    Console.Clear();
+
+    // if the inputted value is not the sentinel value, process it
+    while (value >= 0)
+    {
+      // prompt the user to enter an integer
+      Console.Write("Enter a positive integer (-1 to stop): ");
+      value = Convert.ToInt32(Console.ReadLine());
+
+      // Calculate the running total
+      sum += value; // same as sum = sum + value;
+
+      // Keep track of the number of inputted values
+      count++; // same as count = count + 1;
+    }
+    // Calculate average (only if the user inputted any numbers)
+    if (count > 0)
+    {
+      average = sum / (1.0 * count);
+
+    }
+    else
+    {
+      average = 0;
+
+    }
+    // Print results
+    Console.WriteLine("sum = {0}, count = {1}", sum, count);
+    Console.WriteLine("average = {0:F2} ", average);
+  }
+}
---
+++ Lab4_2/Program.cs
@@ -0,0 +1,65 @@
+﻿﻿/*
+* Name: Onur (Honor) Onel
+* Student Number: 0865803
+* Date: 2024-09-27
+* Lab 4, Part 2
+*
+* Program Description: This program uses a loop and if statement to input marks
+* between 0 and 100 from a user, determines if the mark is pass or fail
+* and outputs the percentage of passing and failing marks. The user then
+* terminates the program by entering a negative value for a mark.
+*/
+
+public static class Lab4_2
+{
+   public static void Main()
+   {
+      Console.Clear();
+      const int PASS = 50;
+      int numPass = 0, numFail = 0, totalMarks = 0;
+      double mark, perPass = 0, perFail = 0;
+      // loop to read in a valid mark or the sentinel value
+      do
+      {
+         // Read initial mark (seed the loop)
+         Console.Write("Enter a mark between 0 and 100 (-ve value to stop): ");
+         mark = Convert.ToDouble(Console.ReadLine());
+      } while (mark > 100);
+      // if the inputted mark is not the sentinel value, process it
+      while (mark >= 0)
+      {
+         // increment the counter for the total number of data values
+         totalMarks++;
+         // Determine if the mark is a pass or fail (If statement)
+         if (mark >= PASS)
+         {
+            numPass++;
+         }
+         else
+         {
+            numFail++;
+         }
+         // Read next mark
+         Console.Write("Enter a mark between 0 and 100 (-ve value to stop): ");
+         mark = Convert.ToDouble(Console.ReadLine());
+      }
+      if (numFail + numPass == 0)
+      {
+         Console.WriteLine("Total number of marks = {0}", totalMarks);
+         Console.WriteLine("Percentage of passing marks = 0.0%");
+         Console.WriteLine("Percentage of failing marks = 0.0%");
+         return;
+      }
+
+      // Calculate the percentage of marks that were passes and fails
+      double failPercentage = (double)numFail / totalMarks;
+      double passPercentage = (double)numPass / totalMarks;
+
+      // Print results
+      Console.WriteLine("Total number of marks = {0}", totalMarks);
+      Console.WriteLine("Total number of pass = {0}", numPass);
+      Console.WriteLine("Total number of fail = {0}", numFail);
+      Console.WriteLine("Percentage of passing marks = {0:P1}", passPercentage);
+      Console.WriteLine("Percentage of failing marks = {0:P1}", failPercentage);
+   }
+}
